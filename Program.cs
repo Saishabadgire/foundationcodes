07eb9dbe977ab54fc5972fccc42c5b8713f25a02
Saisha Badgire <saishabadgire06@gmail.com>
2025-05-10 17:18:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student lobjs1 = new student();
+            lobjs1.printinfo();
+
+
+
+            Console.Write("enter value for name:");
+            string lstrname = Console.ReadLine();
+            Console.Write("enter value for id:");
+            int lintID =Convert.ToInt32 (Console.ReadLine());
+
+            student lobjs2 = new student(lintID,lstrname,"23456****");
+            lobjs2.printinfo();
+
+
+
+           student person1 = new student( 25 , "Alice");
+            person1.Introduce();
+
+
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    public  class student
+    {
+        int id;
+        string name;
+        DateTime dob;
+        string mobile;
+
+
+        public student()             // deafault 
+        {
+            id = 0;
+            name = "saisha";
+            mobile = "unlnown";
+        }
+
+        public student(int aintid, string astrname , string astrmobile)      // parameterised
+        {
+            id = aintid;
+            name = astrname;
+            mobile = astrmobile;
+        }
+
+        public  void printinfo()
+        {
+            Console.WriteLine("id :" + id);
+            Console.WriteLine("name :" + name);
+            Console.WriteLine("mobile :" + mobile);
+        }
+
+
+
+        //2
+        public string nam;
+        public int age;
+
+        // Constructor
+        public student( int personAge , string personName)
+        {
+            nam = personName;
+            age = personAge;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {nam} and I am {age} years old.");
+        }
+
+        
+    }
+}
